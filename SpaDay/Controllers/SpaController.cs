f4201a0f3c44fda2698023875f164ba6fc070a00
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-10-05 20:31:19 -0500

    SpaController

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -42,6 +42,7 @@ namespace SpaDay.Controllers
         [Route("/spa")]
         public IActionResult Menu(string name, string skintype, string manipedi)
         {
+            //List<string> name = new List<string>();
             List<string> facials = new List<string>()
             {
                 "Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"
@@ -55,6 +56,11 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+            ViewBag.name = name;
+            ViewBag.skintype = skintype;   
+            ViewBag.manipedi = manipedi;
+            ViewBag.appropriateFacials = appropriateFacials;
+           
             return View();
         }
 
