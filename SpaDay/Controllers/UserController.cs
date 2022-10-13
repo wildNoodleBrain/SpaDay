5c5681a2a3c771d1a0a13b76a07c1f40dffad9e7
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-10-12 20:27:53 -0500

    Working on IActionResult SubmitAddUserForm

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+                       
+            return View();
+        }
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            EventData.
+
+        }
+    }
+}
